0f70769749e1947c104c6e0b52fdade933d2b1f5
Zoran Bajic <zoranbajic@protonmail.com>
2021-03-13 23:05:35 -0600

    feat: add navbar

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as Etebase from 'etebase';
-import App from './components/App';
-import Login from './components/Login';
+import App from './app';
 import store from './store';
 import { Provider } from 'react-redux';
-import './styles.css';
+import { Router } from 'react-router-dom';
 
 ReactDOM.render(
   <Provider store={store}>
-    <Login />
+    <Router>
+      <App />
+    </Router>
   </Provider>,
   document.getElementById('app') // make sure this is the same as the id of the div in the index.html
 );
