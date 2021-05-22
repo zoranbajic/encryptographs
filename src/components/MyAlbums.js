d2eb022eada8a49326730032e7a860cfd9c3f49b
Zoran Bajic <zoranbajic@protonmail.com>
2021-05-21 21:49:20 -0500

    chore: work on album view component

---
+++ src/components/MyAlbums.js
@@ -0,0 +1,99 @@
+import React, { useState, useContext } from 'react';
+import * as Etebase from 'etebase';
+import Avatar from '@material-ui/core/Avatar';
+import Backdrop from '@material-ui/core/Backdrop';
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Container from '@material-ui/core/Container';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Link from '@material-ui/core/Link';
+import { Link as RouterLink } from 'react-router-dom';
+import PhotoAlbumOutlinedIcon from '@material-ui/icons/PhotoAlbumOutlined';
+import { makeStyles } from '@material-ui/core/styles';
+import MuiAlert from '@material-ui/lab/Alert';
+import Snackbar from '@material-ui/core/Snackbar';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+
+function Copyright() {
+  return (
+    <Typography variant='body2' color='textSecondary' align='center'>
+      {'Copyright © '}
+      <Link color='inherit' href='https://www.encryptographs.com'>
+        Encryptographs
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 1,
+    color: '#fff',
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function MyAlbums() {
+  const classes = useSTyles();
+
+  async function CreateAlbum(evt) {
+    alert('This button works!');
+  }
+
+  return (
+    <Container component='main' maxWidth='xs'>
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <PhotoAlbumOutlinedIcon />
+        </Avatar>
+        <Typography component='h1' variant='h5'>
+          My Albums
+        </Typography>
+        <form className={classes.form} onSubmit={Submit}>
+          <Button
+            type='submit'
+            fullWidth
+            variant='contained'
+            color='primary'
+            className={classes.submit}
+          >
+            Create Album
+          </Button>
+        </form>
+      </div>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+      {/* <Backdrop className={classes.backdrop} open={showProgress}>
+        <CircularProgress color='primary' />
+      </Backdrop>
+      <Snackbar open={showError} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity='error'>
+          Your username or password is incorrect.
+        </Alert>
+      </Snackbar> */}
+    </Container>
+  );
+}
