8d44dd928b311f8097641c3a6581b53a32eba57c
Zoran Bajic <zoranbajic@protonmail.com>
2021-07-23 14:34:45 -0500

    chore: add button to upload images

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import { DrawerMenu } from '.';
 import ImageGallery from 'react-image-gallery';
 import 'react-image-gallery/styles/css/image-gallery.css';
+import { Button, Grid } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { UserContext, UserSessionContext } from '../store';
 
-const Gallery = () => {
+const useStyles = makeStyles((theme) => ({
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function Gallery() {
+  const classes = useStyles();
   const images = [
     {
       original: 'https://picsum.photos/id/1018/1000/600/',
@@ -12,13 +24,34 @@ const Gallery = () => {
       original: 'https://picsum.photos/id/1015/1000/600/',
       thumbnail: 'https://picsum.photos/id/1015/250/150/',
     },
-    {
-      original: 'https://picsum.photos/id/1019/1000/600/',
-      thumbnail: 'https://picsum.photos/id/1019/250/150/',
-    },
   ];
 
-  return <ImageGallery items={images} />;
-};
+  const history = useHistory();
+  const [userSession, setUserSession] = useContext(UserSessionContext);
 
-export default Gallery;
+  // If the user is not logged in, send them to the login page
+  if (!userSession) {
+    history.push('/login');
+  } else {
+  }
+
+  return (
+    <Grid container justifyContent='center'>
+      <Grid container item justifyContent='center'>
+        <ImageGallery items={images} />
+      </Grid>
+      <Grid container item justifyContent='center'>
+        <Button
+          variant='contained'
+          color='primary'
+          className={classes.submit}
+          onClick={() => {
+            alert('This was clicked');
+          }}
+        >
+          Add Images
+        </Button>
+      </Grid>
+    </Grid>
+  );
+}
