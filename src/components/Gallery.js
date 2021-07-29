6bd91d45a6b6d69e1c62d412a5cbeb797379f7af
Zoran Bajic <zoranbajic@protonmail.com>
2021-07-29 18:03:16 -0500

    chore: encode images as base64 and as a Uint8Array

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,5 +1,7 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
+import * as Etebase from 'etebase';
+import { Base64 } from 'js-base64';
 import ImageGallery from 'react-image-gallery';
 import 'react-image-gallery/styles/scss/image-gallery.scss';
 import {
@@ -44,84 +46,51 @@ export default function Gallery(props) {
   const classes = useStyles();
   const location = useLocation();
   const { name, description, uid } = location.state.album;
-  const images = [
-    {
-      original: 'https://picsum.photos/id/1018/1000/600/',
-      thumbnail: 'https://picsum.photos/id/1018/250/150/',
-    },
-    {
-      original: 'https://picsum.photos/id/1015/1000/600/',
-      thumbnail: 'https://picsum.photos/id/1015/250/150/',
-    },
-    // {
-    //   original: 'https://picsum.photos/id/1018/1000/600/',
-    //   thumbnail: 'https://picsum.photos/id/1018/250/150/',
-    // },
-    // {
-    //   original: 'https://picsum.photos/id/1015/1000/600/',
-    //   thumbnail: 'https://picsum.photos/id/1015/250/150/',
-    // },
-    // {
-    //   original: 'https://picsum.photos/id/1018/1000/600/',
-    //   thumbnail: 'https://picsum.photos/id/1018/250/150/',
-    // },
-    // {
-    //   original: 'https://picsum.photos/id/1015/1000/600/',
-    //   thumbnail: 'https://picsum.photos/id/1015/250/150/',
-    // },
-    // {
-    //   original: 'https://picsum.photos/id/1018/1000/600/',
-    //   thumbnail: 'https://picsum.photos/id/1018/250/150/',
-    // },
-    // {
-    //   original: 'https://picsum.photos/id/1015/1000/600/',
-    //   thumbnail: 'https://picsum.photos/id/1015/250/150/',
-    // },
-    // {
-    //   original: 'https://picsum.photos/id/1018/1000/600/',
-    //   thumbnail: 'https://picsum.photos/id/1018/250/150/',
-    // },
-    // {
-    //   original: 'https://picsum.photos/id/1015/1000/600/',
-    //   thumbnail: 'https://picsum.photos/id/1015/250/150/',
-    // },
-    // {
-    //   original: 'https://picsum.photos/id/1018/1000/600/',
-    //   thumbnail: 'https://picsum.photos/id/1018/250/150/',
-    // },
-    // {
-    //   original: 'https://picsum.photos/id/1015/1000/600/',
-    //   thumbnail: 'https://picsum.photos/id/1015/250/150/',
-    // },
-    // {
-    //   original: 'https://picsum.photos/id/1015/1000/600/',
-    //   thumbnail: 'https://picsum.photos/id/1015/250/150/',
-    // },
-    // {
-    //   original: 'https://picsum.photos/id/1018/1000/600/',
-    //   thumbnail: 'https://picsum.photos/id/1018/250/150/',
-    // },
-    // {
-    //   original: 'https://picsum.photos/id/1015/1000/600/',
-    //   thumbnail: 'https://picsum.photos/id/1015/250/150/',
-    // },
-    // {
-    //   original: 'https://picsum.photos/id/1018/1000/600/',
-    //   thumbnail: 'https://picsum.photos/id/1018/250/150/',
-    // },
-    // {
-    //   original: 'https://picsum.photos/id/1015/1000/600/',
-    //   thumbnail: 'https://picsum.photos/id/1015/250/150/',
-    // },
-  ];
+  const [images, setImages] = useState([]);
 
   const history = useHistory();
   const [userSession, setUserSession] = useContext(UserSessionContext);
+
+  console.log('Gallery - Your pictures are - ', images);
 
   // If the user is not logged in, send them to the login page
   if (!userSession) {
     history.push('/login');
   } else {
+  }
+
+  // This function encodes the images both in base64 to use for the image
+  // gallery and as Uint8Arrays to save as items
+  function encodeImages(evt) {
+    const selectedFiles = Array.from(evt.target.files);
+    let base64EncodedImage = '';
+    // We set the input value to an empty string in case the user wants to
+    // select the same files, otherwise the onChange won't fire
+    evt.target.value = '';
+    if (selectedFiles.length > 0) {
+      for (let i = 0; i < selectedFiles.length; i++) {
+        console.log('Gallery - File', selectedFiles[i].name);
+        let fileReader = new FileReader();
+        let imageFile = selectedFiles[i];
+
+        fileReader.onloadend = function () {
+          base64EncodedImage = fileReader.result;
+          // We add to the image array using a wrapper function
+          setImages((images) => [
+            ...images,
+            {
+              original: base64EncodedImage,
+              thumbnail: base64EncodedImage,
+            },
+          ]);
+
+          // We use the Base64 library to convert the image to a Uint8Array
+          const anArray = Base64.toUint8Array(base64EncodedImage);
+        };
+        // After a file is read it triggers the .onloadend function above
+        fileReader.readAsDataURL(imageFile);
+      }
+    }
   }
 
   return (
@@ -133,7 +102,15 @@ export default function Gallery(props) {
             <Typography variant='h4'>{name}</Typography>
           </Grid>
           <Grid container item justifyContent='center'>
-            <ImageGallery items={images} />
+            {images.length === 0 ? (
+              <ImageGallery
+                items={images}
+                showFullscreenButton={false}
+                showPlayButton={false}
+              />
+            ) : (
+              <ImageGallery items={images} />
+            )}
           </Grid>
           <Grid container item justifyContent='center'>
             <label htmlFor='upload-images'>
@@ -142,6 +119,8 @@ export default function Gallery(props) {
                 id='upload-images'
                 name='upload-images'
                 type='file'
+                onChange={encodeImages}
+                multiple
               />
               <Button variant='contained' color='primary' component='span'>
                 Add Images
